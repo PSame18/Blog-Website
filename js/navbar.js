6ce056433aa916630a29771d71d185e20712b407
lopkart <37088422+lopkart@users.noreply.github.com>
2019-06-23 22:54:05 +0200

    Navbar transparent, buttons edited, new bg, jumbotron class for editing header

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,4 @@
 addLoadEvent(function () {
-    $(document).ready(function() {
-        $(".menu-icon").on("click", function () {
-            $("nav ul").toggleClass("showing");
-        });
-    });
-
     $(window).on("scroll", function() {
         if($(window).scrollTop()){
             $('nav').addClass('black');
